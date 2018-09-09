4a1093818164c9b452b530cf507fd7b81401ffb2
“linjunjj” <1045735402@qq.com>
2018-09-09 09:39:35 +0800

    图片资源

---
+++ src/main/java/com/linjun/Main.java
@@ -0,0 +1,32 @@
+package com.linjun;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author 林俊
+ * @create 2018/9/9.
+ * @desc
+ **/
+public class Main {
+ public static  void main(String[] args){
+     System.out.println("启动图片处理");
+     ExecutorService executorService= Executors.newCachedThreadPool();
+     try {
+         Connection connection= Nats.connect("nats://nats.databases.svc.cluster.local:4222");
+         System.out.println("nats://nats.databases.svc.cluster.local:4222");
+         connection.subscribe("channels.dealwith.*",)
+
+     }catch (IOException e){
+         System.out.println("连接nats服务器失败");
+         e.printStackTrace();
+     }
+
+ }
+
+
+}

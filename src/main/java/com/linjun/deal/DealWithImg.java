90a1e24b762eda66c5491a9d5b2ccc590680cd4f
“linjunjj” <1045735402@qq.com>
2018-09-09 11:04:37 +0800

    添加处理功能

--- src/main/java/com/linjun/deal/DealWithImg.java
+++ src/main/java/com/linjun/deal/DealWithImg.java
@@ -50,10 +50,61 @@ public class DealWithImg implements DealWithInterface {
 
         return result;
     }
+//测试部分
+    /**
+     * 多车牌识别
+     * @param mat
+     * @return
+     */
+    public static String[] mutiPlateRecognise(opencv_core.Mat mat){
+        PlateDetect plateDetect = new PlateDetect();
+        plateDetect.setPDLifemode(true);
+        Vector<opencv_core.Mat> matVector = new Vector<opencv_core.Mat>(10);
+        if (0 == plateDetect.plateDetect(mat, matVector)) {
+            CharsRecognise cr = new CharsRecognise();
+            String[] results=new String[matVector.size()];
+            for (int i = 0; i < matVector.size(); ++i) {
+                String result = cr.charsRecognise(matVector.get(i));
+                results[i]=result;
+            }
+            return results;
+        }
+        return null;
+    }
+
+    public static void main(String[] args){
+        int sum=100;
+        int errNum=0;
+        int sumTime=0;
+        long longTime=0;
+        for(int i=sum;i>0;i--){
+            String imgPath = "res/image/test_image/plate_judge.jpg";
+            opencv_core.Mat src = opencv_imgcodecs.imread(imgPath);
+            long now =System.currentTimeMillis();
+            String ret=plateRecognise(src);
+            System.err.println(ret);
+            long s=System.currentTimeMillis()-now;
+            if(s>longTime){
+                longTime=s;
+            }
+            sumTime+=s;
+            if(!"川A0CP56".equals(ret)){
+                errNum++;
+            }
+        }
+        System.err.println("总数量："+sum);
+        System.err.println("单次最长耗时："+longTime+"ms");
+
+        BigDecimal errSum=new BigDecimal(errNum);
+        BigDecimal sumNum=new BigDecimal(sum);
+        BigDecimal c=sumNum.subtract(errSum).divide(sumNum).multiply(new BigDecimal(100));
+        System.err.println("总耗时："+sumTime+"ms,平均处理时长："+sumTime/sum+"ms,错误数量："+errNum+"，正确识别率："+c+"%");
+    }
+
 
     private  String recognition(Map<String,String> datas){
 //        获取识别图片的路径
-         String path=datas.get("path");
+         String path=currentURL()+datas.get("path");
 //         识别车牌场景类型
          String type=datas.get("type");
            long longTime=0;
@@ -68,5 +119,11 @@ public class DealWithImg implements DealWithInterface {
         System.err.println("单次最长耗时："+longTime+"ms");
         return  ret;
     }
+    private  String currentURL(){
+        System.getProperty("user.dir");
+
+        return  System.getProperty("user.dir");
+    }
+
 
 }
